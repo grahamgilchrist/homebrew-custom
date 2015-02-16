24a0081d349da6f4ce548c5391208a328e44fee1
Graham Gilchrist <mail@grahamgilchrist.com>
2015-02-16 17:08:16 +0000

    Add vagrant-boot2docker shell script

---
+++ vagrant-boot2docker.rb
@@ -0,0 +1,20 @@
+class VagrantBoot2Docker < Formula
+  homepage "https://www.github.com/grahamgilchrist/vagrant-boot2docker"
+  url "https://github.com/grahamgilchrist/vagrant-boot2docker", :using => :git, :tag => "0.0.1"
+
+  ## Dependencies
+  depends_on :cask => 'vagrant'
+
+  ## The install method.
+
+  def install
+    # If your formula's build system is not thread safe:
+    ENV.deparallelize
+
+    # No "make", "install" available?
+    bin.install "vagrant-boot2docker"
+
+  end
+end
+
+__END__
