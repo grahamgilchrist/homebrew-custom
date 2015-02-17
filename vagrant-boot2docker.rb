98b104baec78e7e7172bcb9fb966c6e6fa30712f
Graham Gilchrist <mail@grahamgilchrist.com>
2015-02-17 13:09:10 +0000

    Update vagrant-boot2docker.rb

--- vagrant-boot2docker.rb
+++ vagrant-boot2docker.rb
@@ -3,7 +3,7 @@ require 'formula'
 class VagrantBoot2docker < Formula
   homepage 'https://www.github.com/grahamgilchrist/vagrant-boot2docker'
   url 'https://github.com/grahamgilchrist/vagrant-boot2docker/archive/0.0.4.tar.gz'
-  sha1 'caeccf320a42dda3abcfdb4417c7245feb7a118b'
+  sha1 '78ce0fdc10d2c73bcf7d3bd8c83c8f093692dde6'
 
   ## The install method.
   def install
