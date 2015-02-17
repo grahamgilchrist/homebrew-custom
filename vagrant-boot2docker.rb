fb8a7e85842ad43241b75a48843cdddd7b5eb968
Graham Gilchrist <mail@grahamgilchrist.com>
2015-02-17 10:21:31 +0000

    Update vagrant-boot2docker.rb

--- vagrant-boot2docker.rb
+++ vagrant-boot2docker.rb
@@ -11,10 +11,10 @@ class VagrantBoot2docker < Formula
 
   def install
     # If your formula's build system is not thread safe:
-    #ENV.deparallelize
+    ENV.deparallelize
 
     # No "make", "install" available?
-    #bin.install "vagrant-boot2docker"
+    bin.install "vagrant-boot2docker"
 
   end
 end
